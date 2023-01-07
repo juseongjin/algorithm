6676ac95192c7608a2b8276850a4614c7db48174
juseongjin <cyse3062@naver.com>
2023-01-07 23:59:10 +0900

    first commit

---
+++ greedy/guitar_string.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF=987654321;
+int n,m, result, tmp1=INF, tmp2=INF;
+
+int main(){
+    cin >> n >> m;
+    for(int i=0; i<m; i++){
+        int x, y;
+        cin >> x >> y;
+        tmp1 = min(tmp1, x);
+        tmp2 = min(tmp2, y);
+    }
+    cout << tmp1 << "           " << tmp2 << endl;
+    if(tmp1>tmp2*6)result+=tmp2*n;
+    else{
+        result = min((n/6)*tmp1+tmp2*(n%6), ((n/6)+1)*tmp1);
+    }
+    cout << result << endl;
+    return 0;
+}
